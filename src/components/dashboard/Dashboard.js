a9f6d3a5f6602b4f62ed174a30de17cbc19c7daa
JasmineTrinh <61105437+JasmineTrinh@users.noreply.github.com>
2020-06-18 22:24:09 +0700

    update Dashboard (import Navbar)

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,117 +1,114 @@
-import React from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import Button from '@material-ui/core/Button';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import PeopleIcon from '@material-ui/icons/People';
-import BarChartIcon from '@material-ui/icons/BarChart';
-import ItemTable from './ItemTable'
-import { BrowserRouter as Router, Route, Switch, useRouteMatch } from "react-router-dom";
-import { Link as RouterLink } from "react-router-dom"
-import Report from './Report'
-import ManageStaff from './ManageStaff'
-import StaffList from './StaffList'
+import React from "react";
+import clsx from "clsx";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import PeopleIcon from "@material-ui/icons/People";
+import BarChartIcon from "@material-ui/icons/BarChart";
+import ItemTable from "./ItemTable";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useRouteMatch,
+} from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
+import Report from "./Report";
+import ManageStaff from "./ManageStaff";
+import Navbar from "../Navbar";
 
 const drawerWidth = 240;
 
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: 'flex',
+//   },
+//   toolbar: {
+//     paddingRight: 24, // keep right padding when drawer closed
+//   },
+//   toolbarIcon: {
+//     display: 'flex',
+//     alignItems: 'center',
+//     justifyContent: 'flex-end',
+//     padding: '0 8px',
+//     ...theme.mixins.toolbar,
+//   },
+//   appBar: {
+//     zIndex: theme.zIndex.drawer + 1,
+//     transition: theme.transitions.create(['width', 'margin'], {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.leavingScreen,
+//     }),
+//   },
+//   appBarShift: {
+//     marginLeft: drawerWidth,
+//     width: `calc(100% - ${drawerWidth}px)`,
+//     transition: theme.transitions.create(['width', 'margin'], {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.enteringScreen,
+//     }),
+//   },
+//   menuButton: {
+//     marginRight: 36,
+//   },
+//   menuButtonHidden: {
+//     display: 'none',
+//   },
+//   title: {
+//     flexGrow: 1,
+//   },
+//   drawerPaper: {
+//     position: 'relative',
+//     whiteSpace: 'nowrap',
+//     width: drawerWidth,
+//     transition: theme.transitions.create('width', {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.enteringScreen,
+//     }),
+//   },
+//   drawerPaperClose: {
+//     overflowX: 'hidden',
+//     transition: theme.transitions.create('width', {
+//       easing: theme.transitions.easing.sharp,
+//       duration: theme.transitions.duration.leavingScreen,
+//     }),
+//     width: theme.spacing(7),
+//     [theme.breakpoints.up('sm')]: {
+//       width: theme.spacing(9),
+//     },
+//   },
+//   appBarSpacer: theme.mixins.toolbar,
+//   content: {
+//     flexGrow: 1,
+//     height: '100vh',
+//     overflow: 'auto',
+//   },
+//   paper: {
+//     padding: theme.spacing(2),
+//     display: 'flex',
+//     overflow: 'auto',
+//     flexDirection: 'column',
+//   },
+// }));
 
 export default function Dashboard(props) {
-  let { path, url } = useRouteMatch();
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+  // let { path, url } = useRouteMatch();
+  // const classes = useStyles();
+  // const [open, setOpen] = React.useState(true);
+  // const handleDrawerOpen = () => {
+  //   setOpen(true);
+  // };
   const handleDrawerClose = () => {
     setOpen(false);
   };
-
 
   const mainListItems = (
     <div>
@@ -119,25 +116,31 @@ export default function Dashboard(props) {
         <ListItemIcon>
           <BarChartIcon />
         </ListItemIcon>
-        <RouterLink to={`${url}/report`}><ListItemText primary="Reports" /></RouterLink>
+        <RouterLink to={`${url}/report`}>
+          <ListItemText primary="Reports" />
+        </RouterLink>
       </ListItem>
-      <ListItem button >
+      <ListItem button>
         <ListItemIcon>
           <DashboardIcon />
         </ListItemIcon>
-        <RouterLink to={`${url}/editMenu`}><ListItemText primary="Edit Menu" /></RouterLink>
+        <RouterLink to={`${url}/editMenu`}>
+          <ListItemText primary="Edit Menu" />
+        </RouterLink>
       </ListItem>
       <ListItem button>
         <ListItemIcon>
           <ShoppingCartIcon />
         </ListItemIcon>
-        <RouterLink to={`${url}/viewStaff`}><ListItemText primary="View all staff" /></RouterLink>
+        <ListItemText primary="Sales" />
       </ListItem>
       <ListItem button>
         <ListItemIcon>
           <PeopleIcon />
         </ListItemIcon>
-        <RouterLink to={`${url}/addStaff`}><ListItemText primary="Add Staff" /></RouterLink>
+        <RouterLink to={`${url}/addStaff`}>
+          <ListItemText primary="Add Staff" />
+        </RouterLink>
       </ListItem>
     </div>
   );
@@ -145,30 +148,7 @@ export default function Dashboard(props) {
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-        <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-            {props.name}
-          </Typography>
-          <IconButton color="inherit">
-            <Button
-              variant="contained"
-              onClick={() => window.location = '/foodcourt'}
-            >Log Out
-            </Button>
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-
+      <Navbar />
       <Router>
         <Drawer
           variant="permanent"
@@ -199,16 +179,10 @@ export default function Dashboard(props) {
               <ItemTable />
             </main>
           </Route>
-          <Route path={`${path}/viewStaff`}>
-            <main className={classes.content}>
-              <div className={classes.appBarSpacer} />
-              <StaffList />
-            </main>
-          </Route>
           <Route path={`${path}/addStaff`}>
             <main className={classes.content}>
               <div className={classes.appBarSpacer} />
-              <ManageStaff/>
+              <ManageStaff />
             </main>
           </Route>
         </Switch>
