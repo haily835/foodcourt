a775ca9d1ee96a94d9483e2d88129d83baee87dd
giahung127 <66950215+giahung127@users.noreply.github.com>
2020-07-07 17:43:28 +0700

    Update CheckList.compoment.js
    Add the "Delete" button (remove an item from the order) and "Review" button (show the final bill of the order)

--- src/components/itemList/CheckList.compoment.js
+++ src/components/itemList/CheckList.compoment.js
@@ -7,22 +7,16 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import { IconButton, TableFooter } from '@material-ui/core';
+import { IconButton, TableFooter, Button } from '@material-ui/core';
 import AddCircelOutline from '@material-ui/icons/AddCircleOutline';
 import RemoveCircelOutline from '@material-ui/icons/RemoveCircleOutline';
-import Item from './Item.component';
-import {createOrderData} from './ItemList.component'
-import Orders from '../dashboard/Orders';
+import DeleteIconOutline from '@material-ui/icons/DeleteOutline'
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 300,
+    minWidth: 400,
   },
 });
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
 export function thousands_separators(num)
   {
@@ -34,54 +28,20 @@ export function thousands_separators(num)
 export default function CheckList(props) {
   const classes = useStyles();
   const [items, setItems] = useState([]);
-  const [orderPostData, setOrderPostData] = useState({})
-  const [order, setOrder] = useState([])
-  let total = 0;
+
   useEffect(()=>{
-    setItems(props.items)
-    setOrderPostData(props.items, props.customerID)
-    setOrder(props)
+    setItems(props.items.items)
   }, [props])
-
-  const handleIncrement = (_item) => { 
-    props.items.map((item) => {
-      if(_item._id === item._id){
-          item.number++;
-      }
-      console.log(order)
-      setOrderPostData(createOrderData(props.items, props.customerID))  
-    })
-  }
-
-  const handleDecrement = (_item) => {
-    props.items.map((item) => {
-      if(_item._id === item._id && item.number > 0){
-        item.number--;
-        if(item.number === 0){
-          props.items.splice(props.items.indexOf(item), 1);
-        }
-        console.log(order)
-        setOrderPostData(createOrderData(props.items, props.customerID))
-      }  
-    })
-  }
-
-  const calculateTotal = () => {
-    props.items.map((item) => {
-      total += item.price * item.number;
-    })
-    return total
-  }
 
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell align="left">Dessert</TableCell>
+            <TableCell align="left">Name</TableCell>
             <TableCell align="center">Number</TableCell>
             <TableCell align="left">Price</TableCell>
-            <TableCell align="right">Total</TableCell>
+            <TableCell align="left">Total</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -90,15 +50,19 @@ export default function CheckList(props) {
               <TableCell align="left">{item.name}</TableCell>
               <TableCell align="center">
               <IconButton> 
-                <RemoveCircelOutline onClick={() => handleDecrement(item)}/>
+                <RemoveCircelOutline onClick={() => props.handleDecrement(item)}/>
               </IconButton>
               {item.number}
               <IconButton> 
-                <AddCircelOutline onClick={() => handleIncrement(item)}/>
+                <AddCircelOutline onClick={() => props.handleIncrement(item)}/>
               </IconButton>
               </TableCell>
               <TableCell align="left">{thousands_separators(item.price)}</TableCell>
-              <TableCell align="right">{thousands_separators(item.number * item.price)}</TableCell>
+              <TableCell align="right">{thousands_separators(item.number * item.price)}
+              <IconButton> 
+                <DeleteIconOutline onClick={() => props.handleDelete(item)}/>
+              </IconButton>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
@@ -107,7 +71,7 @@ export default function CheckList(props) {
             <TableCell align="center"><h1><b> Total:  </b></h1></TableCell>
             <TableCell></TableCell>
             <TableCell></TableCell>
-            <TableCell align="right"><h1><b> { thousands_separators(calculateTotal()) }</b></h1></TableCell>
+            <TableCell align="right"><h1><b> {thousands_separators(props.items.total)}</b></h1></TableCell>
           </TableRow>
         </TableFooter>
       </Table>
