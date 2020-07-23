c6067b0f4f9d6c18eb7fa0fa0b413d695d47e128
nguyenbao2301 <66950291+nguyenbao2301@users.noreply.github.com>
2020-07-23 19:29:02 +0700

    change employee list

---
+++ src/components/Vendor/data/employee.js
@@ -0,0 +1,64 @@
+export const employees = [
+    {
+        id:1,
+        text:"Elouise Frame",
+        phone:"0123456781",
+        email:"Fram12@gmail.com"
+    },
+    
+    {
+        id:2,
+        text: "Erin Christian",
+        phone:"2423529501",
+        email:"erin_christ@ymail.net"
+    },
+
+    {
+        id:3,
+        text: "Caius Devlin",
+        phone:"2423335300",
+        email:"Devlinnn@hmail.org"
+    },
+    {
+        id:4,
+        text:"Ivor Murray",
+        phone:"228870647",
+        email:"murrayivor@yah.com"
+    },
+    {
+        id:5,
+        text:"Britany Huffman",
+        phone:"3558597188",
+        email:"huffman2306@yaf.com"
+    },
+    {
+        id:6,
+        text:"Aayan Acosta",
+        phone:"424568852",
+        email:"aaaayan@kmail.com"
+    },
+    {
+        id:7,
+        text:"Mustafa Gill",
+        phone:"2466142830",
+        email:"gillmust_1@hme.us"
+    },
+    {
+        id:8,
+        text:"Esa Fenton",
+        phone:"6780354460",
+        email:"esa_fenton34@uif.com"
+    },
+    {
+        id:9,
+        text:"Orlaith Herring",
+        phone:"1245910635",
+        email:"herring_o@gmail.com"
+    },
+    {
+        id:10,
+        text:"Zahid Hinton",
+        phone:"3026880553",
+        email:"blue_zahid@yahoo.net"
+    },
+]
